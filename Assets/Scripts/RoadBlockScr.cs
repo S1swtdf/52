bf712c3bf5cc288a7f206ac123e0d9dcf0a19c85
S1swtdf <zxfa.com@gmail.com>
2021-12-22 02:59:56 +0200

    1
    1

---
+++ Assets/Scripts/RoadBlockScr.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBlockScr : MonoBehaviour {
+
+    GameManager GM;
+    Vector3 moveVec;
+
+    public GameObject CoinsObj;
+
+    public int CoinChance;
+    bool coinsSpawn;
+    
+
+    public List<GameObject> PowerUps;
+
+	void Start ()
+    {
+        PowerUpController.CoinsPowerUpEvent += CoinsEvent;
+
+        GM = FindObjectOfType<GameManager>();
+        moveVec = new Vector3(-1, 0, 0);
+
+        coinsSpawn = Random.Range(0, 101) <= CoinChance;
+        CoinsObj.SetActive(coinsSpawn);
+
+      
+    }
+	
+	void Update ()
+    {
+        if (GM.CanPlay)
+            transform.Translate(moveVec * Time.deltaTime * GM.CurrentMoveSpeed);
+	}
+
+    void CoinsEvent(bool activate)
+    {
+        if (activate)
+        {
+            CoinsObj.SetActive(true);
+            return;
+        }
+
+        if (!coinsSpawn)
+            CoinsObj.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        PowerUpController.CoinsPowerUpEvent -= CoinsEvent;
+    }
+}
